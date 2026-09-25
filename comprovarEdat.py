a8ecca2553067502d6483c4c67c04a3bd574e22a
JoelM <joel.morado.7ea@itb.cat>
2026-09-25 10:29:13 +0200

    Primer commit i push

---
+++ comprovarEdat.py
@@ -0,0 +1,8 @@
+#By Joel Morado Rueda, @ITB 25/09/2026
+edat = int;
+
+edat = input("Digues quina edat tens:")
+
+if edat >= 18:
+    print ("Ets adult, toca treballar!")
+print("Ets menor, gaudeix :()")
---
+++ holamon.py
@@ -0,0 +1,15 @@
+print("Hola, món :)")
+"""
+
+Joel Morado Rueda
+
+25/09/26
+
+ASIXc1C. MDS
+
+Descripció:
+
+Un test de python
+
+"""
+#Si fa no fa es poden posar més coses.
